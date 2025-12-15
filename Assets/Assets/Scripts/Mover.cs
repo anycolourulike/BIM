40adf568f0cf9d5c271e55c19e1ee706d65d28a5
Bill Molloy <willymolloy@gmail.com>
2025-12-15 23:02:56 +0000

    Mover update

--- Assets/Assets/Scripts/Mover.cs
+++ Assets/Assets/Scripts/Mover.cs
@@ -7,34 +7,44 @@ using System.Collections;
     {  
                
         private NavMeshAgent navMeshAgent;
-        private Vector3 destination;
-        public float maxSpeed;
         Animator anim;
         Rigidbody rb;
 
         public void Start()
         {
             navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+            navMeshAgent.updateRotation = false;
             anim = GetComponent<Animator>();
             rb = GetComponent<Rigidbody>();
+            rb.isKinematic = true;
         }
 
         void Update()
         {           
             UpdateAnimator();  
-        }      
-
-        public float MaxSpeed()
-        {
-            return maxSpeed;
-        }     
+            RotateTowardsVelocity();
+        }  
 
         public void RotateTowards(Transform target)
         {
             int rotSpeed = 160;
             var targetToLook = Quaternion.LookRotation(target.transform.position - this.transform.position);
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetToLook, rotSpeed * Time.deltaTime);            
-        }       
+        }  
+        
+        public void RotateTowardsVelocity()
+        {
+            Vector3 dir = navMeshAgent.desiredVelocity;
+            dir.y = 0;
+
+            if (dir.sqrMagnitude < 0.01f) return;
+
+            Quaternion targetRot = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                targetRot,
+                10f * Time.deltaTime);
+        }
 
         public void StartMoveAction(Vector3 destination, float speedFraction)
         {
@@ -45,24 +55,18 @@ using System.Collections;
         {   
             navMeshAgent.isStopped = false;      
             navMeshAgent.destination = destination; 
-            navMeshAgent.speed = maxSpeed * Mathf.Clamp01(speedFraction);
+            //navMeshAgent.speed = maxSpeed * Mathf.Clamp01(speedFraction);
         }
 
         public void CancelNav()
         {            
             navMeshAgent.isStopped = true;
-            rb.velocity = Vector3.zero;
+            navMeshAgent.ResetPath();
         }
                
         private void UpdateAnimator()
         {
-            anim.SetFloat("Locomotion", maxSpeed);         
-        }
-
-
-        IEnumerator NavMeshDelay()
-        {
-            yield return new WaitForSeconds(1f);
-            navMeshAgent.enabled = true;
-        }        
+            float speed = navMeshAgent.velocity.magnitude;
+            anim.SetFloat("Locomotion", speed);         
+        }       
     }
